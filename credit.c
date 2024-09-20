6d8e6a0b8fd08888f717b9339a88908b67f26c08
Omzlad123 <144887862+Omzlad123@users.noreply.github.com>
2024-09-20 21:12:22 +0300

    Update credit.c

--- credit.c
+++ credit.c
@@ -1,65 +1,76 @@
 #include <cs50.h>
-#include <math.h>
 #include <stdio.h>
+
 int main(void)
-{ 
-    // Ask for input
-    long long cardnumber;
-    do
+{
+  long type=0;
+  long Num = get_long("Number??");
+  int i= 0;
+  int y;
+  int sum=0;
+  while (Num >0)
+  {
+       if (Num == 34 ||Num ==  37)
     {
-        cardnumber = get_long_long("Number: ");
+      type = 1;
     }
-    while (cardnumber < 0);
-    //Count the number of digits 
-    int count = 0;
-    long long i = cardnumber;
-    while (i > 0)
+    else if (Num == 4)
     {
-        i = i / 10;
-        count++;
+      type = 2;
     }
-    //Multiply every other digit by 2, starting with the number’s second-to-last digit, and then add those products’ digits together. Add the sum to the sum of the digits that weren’t multiplied by 2.
-    long long j = cardnumber;
-    int sum1 = 0; 
-    int remainder1;
-    int remainder0;
-    while (j != 0)
+    else if (Num == 51 || Num == 52 ||Num ==  53 ||Num ==  54 || Num == 55)
+
     {
-        remainder1 = 2 * ((j % 100) / 10);
-        remainder0 = remainder1 % 10 + round(remainder1 / 10);
-        sum1 = sum1 + remainder0 ;
-        j = j / 100;
+      type = 3;
     }
-    //Add the sum to the sum of the digits that weren’t multiplied by 2.
-    long long k = cardnumber;
-    int sum2 = 0; 
-    int remainder2;
-    while (k != 0)
+    int x = Num%10;
+    Num = Num/10;
+           if (Num == 34 ||Num ==  37)
     {
-        remainder2 = k % 10;
-        sum2 = sum2 + remainder2;
-        k = k / 100;
+      type = 1;
     }
-    int checksum = sum1 + sum2;
-    //If the total’s last digit is 0 (or, put more formally, if the total modulo 10 is congruent to 0), the number is valid!
-    if (checksum % 10 == 0 && count == 15 && (floor(cardnumber / 10000000000000) == 37 || floor(cardnumber / 10000000000000) == 34))
+    else if (Num == 4)
     {
-        printf("AMEX\n");
+      type = 2;
     }
-    else if (checksum % 10 == 0 && count == 13 && floor(cardnumber / 1000000000000) == 4)
+    else if (Num == 51 || Num == 52 ||Num ==  53 ||Num ==  54 || Num == 55)
+
     {
-        printf("VISA\n");
+      type = 3;
     }
-    else if (checksum % 10 == 0 && count == 16 && floor(cardnumber / 1000000000000000) == 4)
+    i = i +1;
+    if (Num>0)
     {
-        printf("VISA\n");
-    }
-    else if (checksum % 10 == 0 && count == 16 && (floor(cardnumber / 100000000000000) == 51 || floor(cardnumber / 100000000000000) == 52 || floor(cardnumber / 100000000000000) == 53 || floor(cardnumber / 100000000000000) == 54 || floor(cardnumber / 100000000000000) == 55))
-    {
-        printf("MASTERCARD\n");
+      y = ((Num%10)*2);
+      if (y >=10)
+      {
+        y = (y%10) + ((y/10)%10);
+      }
+      i = i+1;
+      Num=Num/10;
+      sum = x + y + sum;
     }
     else
     {
-        printf("INVALID\n");
+      sum = sum + x;
     }
+  }
+
+if (type == 1 && (sum%10 == 0) && i==15)
+{
+  printf("AMEX\n\n");
+}
+else if (type == 2 && (sum%10 == 0) && (i== 13 ||i== 16))
+{
+  printf("Visa\n\n ");
+}
+else if (type == 3 && (sum%10 == 0) && (i==16))
+{
+  printf("MASTERCARD\n\n");
+}
+else
+{
+  printf("INVALID\n\n");
+}
+printf("%ld",type);
 }
